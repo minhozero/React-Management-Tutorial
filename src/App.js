a9f35ee5878a57e0267e2b24772e9f53f38b56f6
Choi Min-ho <minho85zero@naver.com>
2019-04-30 17:50:51 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React,{ Component } from 'react';
-import logo from './logo.svg';
+import Customer from './conponents/Customer'
 import './App.css';
+
+const customers = [
+  {
+    'id': '1',
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '최민호',
+    'birthday': '19851121',
+    'gender': '남자',
+    'job': '직장인'
+  },
+  {
+    'id': '2',
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '16551121',
+    'gender': '남자',
+    'job': '서자'
+  },
+  {
+    'id': '3',
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '15551121',
+    'gender': '남자',
+    'job': '장군'
+  }
+]
 
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
       </div>
     );
   }
