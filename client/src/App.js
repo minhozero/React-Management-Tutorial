69c16b99c17ba87adeeca2e0e13d13be54aba4eb
Choi Min-ho <minho85zero@naver.com>
2019-05-14 16:58:03 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress'; //프로그레스바(로딩)
 import { withStyles } from '@material-ui/core/styles';
+import { setInterval } from 'timers';
 
 
 const styles = theme => ({
@@ -18,25 +20,49 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: { // 9강 추가.
+    margin : theme.spacing.unit * 2
   }
 });
+
+/*
+api 사이클
+1) constructor()
+2) componentWillMount()
+3) render()
+4) componentDidMount()
+*/
+
+/*
+props or state => shouldComponentUpdate()
+*/
 
 class App extends Component {
 
   state = {
-    customers : ""
+    customers : "",
+    completed : 0 // 프로그레스 %
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    /*
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
+    */
   }
 
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed : completed >= 100 ? 0 : completed + 1 });
   }
 
   render(){
@@ -57,7 +83,13 @@ class App extends Component {
           <TableBody>
             { this.state.customers ? this.state.customers.map(c => {
                 return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
-              }) : ""}
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.prgress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+              }
           </TableBody>
         </Table>
       </Paper>
